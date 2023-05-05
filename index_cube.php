e62ead2e967b2d79b5e6dc9a797428abb62e891b
farbod <farbod@strawberry>
2023-05-05 18:19:07 +0100

    init project

---
+++ index_cube.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Welcome to Cube Page</title>
+	<meta charset="utf-8">
+ <!--   <meta name="referrer" content="no-referrer-when-downgrade">
+ -->   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="cube">
+				<div class="top">
+                    <center>
+                        <br>
+                        <br>
+                        <h1>Welcome</h1>
+                        <h1>to</h1>
+                        <h1>Cube Page</h1>
+                        <br>
+                        <br>
+                        <h2 style="color:skyblue">How are you today?</h2>
+                        <h2></h2>
+                    </center>
+                </div>
+        <div>
+            <span style="--i:0;"></span>
+            <span style="--i:1;"></span>
+            <span style="--i:2;"></span>
+            <span style="--i:3;"></span>
+        </div>
+    </div>
+</body>
+
+</html>
